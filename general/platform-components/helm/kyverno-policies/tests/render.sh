bb718ac39f75e5e6dbc75f4b48112efe09cee338
davrad <david@darad.net>
2026-08-19 12:37:06 +0200

    feat: add policy tests for traefik

---
+++ general/platform-components/helm/kyverno-policies/tests/render.sh
@@ -0,0 +1,15 @@
+set -e
+# chart root
+cd "$(dirname "$0")/.."
+
+helm template . -f tests/bestPractices/values.yaml \
+  --show-only templates/bestPractices/disallow-default-namespace.yaml \
+  --show-only templates/bestPractices/disallow-latest-tag.yaml \
+  --show-only templates/bestPractices/require-pod-disruption-budget.yaml \
+  --show-only templates/bestPractices/require-ro-rootfs.yaml \
+  > /tmp/kyverno-policies.yaml
+
+helm template . -f tests/traefik/values.yaml \
+  --show-only templates/traefik/disallow-default-tlsoptions.yaml \
+  --show-only templates/traefik/disallow-default-tlsoptions-in-cel-expressions.yaml \
+  > /tmp/kyverno-traefik.yaml
